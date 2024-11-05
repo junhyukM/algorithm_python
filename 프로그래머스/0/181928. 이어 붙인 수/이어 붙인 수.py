d1dc9c0a00f5ab0f2db84a593e80f58a6c1df826
junhyukM <131353395+junhyukM@users.noreply.github.com>
2024-11-05 21:45:43 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = 0
+    num1 = ''
+    num2 = ''
+    for i in num_list:
+        if i % 2:
+            num1 += str(i)
+        elif i % 2 == 0:
+            num2 += str(i)
+    answer = int(num1) + int(num2)
+    return answer
